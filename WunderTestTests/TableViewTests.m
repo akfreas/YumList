92b7ded2beddca0e85ad21d7dc179391b8430d2f
Alex Freas <akfreas@gmail.com>
2013-11-13 04:20:42 -0500

    [tests] Added tests for tableview, checking for number of rows same as in db.

---
+++ WunderTestTests/TableViewTests.m
@@ -0,0 +1,39 @@
+#import <XCTest/XCTest.h>
+#import "ListTableView.h"
+#import "TestDataGenerator.h"
+#import "PersistenceManager.h"
+#import "ListItem.h"
+@interface TableViewTests : XCTestCase
+
+@end
+
+@implementation TableViewTests {
+    ListTableView *tableView;
+}
+
+- (void)setUp
+{
+    [super setUp];
+    [PersistenceManager deletePersistentStore];
+    tableView = [[ListTableView alloc] initWithFrame:CGRectZero];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+
+-(void)testTableView {
+    
+    NSInteger numRows = arc4random() % 100;
+
+    [TestDataGenerator generateTestListItemDataWithCount:numRows];
+    [tableView reloadData];
+    NSInteger numberOfRows = [tableView numberOfRowsInSection:0];
+    XCTAssertEqual(numberOfRows, numRows, @"Number of rows in tableview are not the same as number of rows in DB.");
+}
+
+@end
