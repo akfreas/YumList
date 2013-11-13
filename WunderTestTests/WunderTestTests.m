121960cba2ccd085401661f064b21bd0ccd96e4b
Alex Freas <akfreas@gmail.com>
2013-11-12 22:25:00 -0500

    Initial Commit

---
+++ WunderTestTests/WunderTestTests.m
@@ -0,0 +1,34 @@
+//
+//  WunderTestTests.m
+//  WunderTestTests
+//
+//  Created by Alexander Freas on 11/12/13.
+//  Copyright (c) 2013 Sashimiblade. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WunderTestTests : XCTestCase
+
+@end
+
+@implementation WunderTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
