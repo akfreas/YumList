1803cc3841027d6df18709a73d7e39873aa73a0f
Alex Freas <akfreas@gmail.com>
2013-11-19 09:15:33 -0500

    [main view] Added autoresizingmask to mainviewcontroller's view.

--- WunderTest/Classes/MainViewController.m
+++ WunderTest/Classes/MainViewController.m
@@ -10,24 +10,18 @@
 -(id)init {
     self = [super init];
     if (self) {
-        [TestDataGenerator generateTestListItemDataWithCount:10];
+        self.view.autoresizesSubviews = YES;
+        self.view.autoresizingMask = (UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleTopMargin);
     }
     return self;
 }
-
--(void)loadView {
-    self.view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 560)];
-//    self.view.backgroundColor = [UIColor orangeColor];
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     tableViewList = [[ListTableView alloc] initWithFrame:self.view.frame];
-
     [self.view addSubview:tableViewList];
+    
     [tableViewList reloadData];
-	// Do any additional setup after loading the view.
 }
 
 - (void)didReceiveMemoryWarning
