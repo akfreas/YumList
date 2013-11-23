b12ea2891c5fc036b66264c0af08a2665916f512
Alex Freas <akfreas@gmail.com>
2013-11-23 15:09:40 -0500

    [data model] Added yumitem data model and object.

---
+++ YumList/Classes/YumItem.h
@@ -0,0 +1,21 @@
+//
+//  YumItem.h
+//  YumList
+//
+//  Created by Alexander Freas on 11/23/13.
+//  Copyright (c) 2013 Sashimiblade. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface YumItem : NSManagedObject
+
+@property (nonatomic, retain) NSString * externalURL;
+@property (nonatomic, retain) NSString * imageURL;
+@property (nonatomic, retain) NSString * title;
+@property (nonatomic, retain) NSString * externalYumID;
+@property (nonatomic, retain) NSDate * syncDate;
+
+@end
---
+++ YumList/Classes/YumItem.m
@@ -0,0 +1,20 @@
+//
+//  YumItem.m
+//  YumList
+//
+//  Created by Alexander Freas on 11/23/13.
+//  Copyright (c) 2013 Sashimiblade. All rights reserved.
+//
+
+#import "YumItem.h"
+
+
+@implementation YumItem
+
+@dynamic externalURL;
+@dynamic imageURL;
+@dynamic title;
+@dynamic externalYumID;
+@dynamic syncDate;
+
+@end
