e521d2cffc3a7397c5259fd13165f96389c06c0a
Alex Freas <akfreas@gmail.com>
2013-11-25 04:18:02 -0500

    [header toolbar] Added title.

--- YumList/Classes/HeaderToolbar.m
+++ YumList/Classes/HeaderToolbar.m
@@ -1,7 +1,10 @@
 #import "HeaderToolbar.h"
+#import "SourceManager.h"
+#import "YumSource.h"
 
 @implementation HeaderToolbar {
     UIBarButtonItem *leftNavigationButton;
+    UILabel *titleLabel;
 }
 
 -(id)initWithFrame:(CGRect)frame {
@@ -10,8 +13,27 @@
         // Initialization code
         self.barTintColor = [UIColor whiteColor];
         self.translucent = NO;
+        self.translatesAutoresizingMaskIntoConstraints = NO;
+        [self addTitleLabelAndConstraints];
+        [SourceManager addYumSourceChangedAction:^(YumSource *newSource) {
+            titleLabel.text = newSource.name;
+        }];
     }
     return self;
+}
+
+-(void)addTitleLabelAndConstraints {
+    
+    if (titleLabel == nil) {
+        titleLabel = [[UILabel alloc] initWithFrame:CGRectZero];
+        [self addSubview:titleLabel];
+        titleLabel.textAlignment = NSTextAlignmentCenter;
+        titleLabel.text = @"Add Some Yums!";
+        titleLabel.textColor = [UIColor blackColor];
+        [self addConstraintWithVisualFormat:@"H:[titleLabel(250)]" bindings:MXDictionaryOfVariableBindings(titleLabel)];
+        [self addConstraintWithVisualFormat:@"V:|[titleLabel(40)]" bindings:MXDictionaryOfVariableBindings(titleLabel)];
+        [self addConstraint:[NSLayoutConstraint constraintWithItem:titleLabel attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeCenterX multiplier:1.0f constant:0]];
+    }
 }
 
 -(void)setLeftNavigationButtonTappedAction:(void (^)())leftNavigationButtonTappedAction {
@@ -24,14 +46,6 @@
         _leftNavigationButtonTappedAction();
     }];
     [itemArray addObject:leftNavigationButton];
-    self.items = [NSArray arrayWithArray:itemArray];
-}
-
--(void)layoutSubviews {
-    [super layoutSubviews];
-    NSMutableArray *itemArray = [NSMutableArray arrayWithArray:self.items];
-    UIBarButtonItem *item = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace handler:NULL];
-    [itemArray insertObject:item atIndex:1];
     self.items = [NSArray arrayWithArray:itemArray];
 }
 
