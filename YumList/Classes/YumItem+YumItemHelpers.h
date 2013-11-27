4e527413560ec9299ef87fd3cb49ab7f4d0829e4
Alex Freas <akfreas@gmail.com>
2013-11-26 22:57:44 -0500

    [persistence helpers] Added some methods that were in ListItem model.

--- YumList/Classes/YumItem+YumItemHelpers.h
+++ YumList/Classes/YumItem+YumItemHelpers.h
@@ -1,4 +1,5 @@
 #import "YumItem.h"
+#import "YumSource.h"
 
 @interface YumItem (YumItemHelpers)
 
@@ -6,6 +7,16 @@
 +(YumItem *)itemWithExternalID:(NSString *)externalID context:(NSManagedObjectContext *)context;
 +(YumItem *)newItemWithDictionary:(NSDictionary *)dict;
 +(YumItem *)newItemWithDictionary:(NSDictionary *)dict context:(NSManagedObjectContext *)context;
++(NSArray *)itemsWithSource:(YumSource *)source;
++(NSArray *)itemsWithSource:(YumSource *)source context:(NSManagedObjectContext *)context;
+
++(void)removeYumItemAndReorderYumItems:(YumItem *)item;
++(void)insertYumItemAndReorderYumItems:(YumItem *)item inContext:(NSManagedObjectContext *)context;
++(void)insertYumItemAndReorderYumItems:(YumItem *)item;
+
++(NSArray *)allObjectsSortedByListOrderInContext:(NSManagedObjectContext *)context;
 
 -(void)fetchImageAndSave:(void (^)(UIImage *))completion;
+-(void)populatePropertiesFromExternalService:(void(^)(YumItem *))completion;
+
 @end
--- YumList/Classes/YumItem+YumItemHelpers.m
+++ YumList/Classes/YumItem+YumItemHelpers.m
@@ -1,6 +1,7 @@
 #import "YumItem+YumItemHelpers.h"
 #import "NSManagedObject+Helpers.h"
 #import "PersistenceManager.h"
+#import "YumSource.h"
 #import <AFNetworking.h>
 
 @implementation YumItem (YumItemHelpers)
@@ -43,6 +44,92 @@
     return newItem;
 }
 
++(void)removeYumItemAndReorderYumItems:(YumItem *)item {
+    
+    NSArray *YumItemSortedFilteredArray = [YumItem YumItemsGreaterThanOrderOfYumItem:item inContext:item.managedObjectContext];
+    
+    for (YumItem *decrementItem in YumItemSortedFilteredArray) {
+        decrementItem.listOrder = [NSNumber numberWithInteger:[decrementItem.listOrder integerValue] - 1];
+    }
+    [item delete];
+}
+
+
++(void)insertYumItemAndReorderYumItems:(YumItem *)item {
+    return [self insertYumItemAndReorderYumItems:item inContext:[PersistenceManager managedObjectContext]];
+}
+
++(void)insertYumItemAndReorderYumItems:(YumItem *)item inContext:(NSManagedObjectContext *)context {
+    
+    if (item.managedObjectContext == nil || item.managedObjectContext != context) {
+        [context insertObject:item];
+    }
+    
+    NSArray *YumItemSortedFilteredArray = [YumItem YumItemsGreaterThanOrderOfYumItem:item inContext:context];
+    int i = [item.listOrder integerValue];
+    for (YumItem *incrementItem in YumItemSortedFilteredArray) {
+        i++;
+        incrementItem.listOrder = [NSNumber numberWithInteger:i];
+    }
+}
+
+
++(NSArray *)YumItemsGreaterThanOrderOfYumItem:(YumItem *)item {
+    return [self YumItemsGreaterThanOrderOfYumItem:item inContext:[PersistenceManager managedObjectContext]];
+}
+
++(NSArray *)YumItemsGreaterThanOrderOfYumItem:(YumItem *)item inContext:(NSManagedObjectContext *)context {
+    NSFetchRequest *request = [YumItem baseFetchRequest];
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"listOrder >= %@ && self != %@", item.listOrder, item];
+    NSSortDescriptor *sortDescriptor = [NSSortDescriptor sortDescriptorWithKey:@"listOrder" ascending:YES];
+    request.predicate = predicate;
+    request.sortDescriptors = @[sortDescriptor];
+    NSError *error = nil;
+    NSArray *itemsMatching = [context executeFetchRequest:request error:&error];
+    if (error != nil) {
+        [NSException raise:YLCoreDataException format:@"Error fetching list item with order %@ in MOC: %@ Error: %@", item, error, context];
+    }
+    return itemsMatching;
+}
+
+
++(NSArray *)allObjectsSortedByListOrderInContext:(NSManagedObjectContext *)context {
+    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:NSStringFromClass(self.class)];
+    NSSortDescriptor *listOrderSortDescriptor = [NSSortDescriptor sortDescriptorWithKey:@"listOrder" ascending:YES];
+    request.sortDescriptors = @[listOrderSortDescriptor];
+    NSError *error = nil;
+    NSArray *sortedObjects = [context executeFetchRequest:request error:&error];
+    NSAssert(error == nil, @"Error when fetching %@. Error: %@", NSStringFromClass(self.class), error);
+    return sortedObjects;
+}
+
+-(void)populatePropertiesFromExternalService:(void(^)(YumItem *))completion {
+    NSURL *requestURL = [NSURL URLWithString:self.externalYumID relativeToURL:[NSURL URLWithString:@"http://api.yummly.com/v1/api/recipe/"]];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:requestURL];
+    [request setValue:@"d3632000" forHTTPHeaderField:@"X-Yummly-App-ID"];
+    [request setValue:@"bac160d28227bda4f6effe7048209270" forHTTPHeaderField:@"X-Yummly-App-Key"];
+    AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+    operation.responseSerializer = [AFJSONResponseSerializer serializer];
+    NSManagedObjectID *ourID = self.objectID;
+    [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, NSDictionary *responseJSON) {
+        PersistenceManager *newManager = [PersistenceManager new];
+        YumItem *ourItem = [YumItem objectWithObjectID:ourID inContext:newManager.managedObjectContext];
+        NSDictionary *source = [responseJSON objectForKey:@"source"];
+        ourItem.externalSiteTitle = source[@"sourceDisplayName"];
+        ourItem.externalURL = source[@"sourceSiteUrl"];
+        ourItem.title = responseJSON[@"name"];
+        [ourItem save];
+        NSManagedObjectID *objID = ourItem.objectID;
+        dispatch_async(dispatch_get_main_queue(), ^{
+            YumItem *mainQueueItem = [YumItem objectWithObjectID:objID];
+            completion(mainQueueItem);
+        });
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        
+    }];
+    [[NSOperationQueue mainQueue] addOperation:operation];
+}
+
 -(void)fetchImageAndSave:(void (^)(UIImage *))completion {
     
     if (self.image == nil && [self.imageURL isEqualToString:@""] == NO) {
@@ -73,6 +160,23 @@
         self.image = [NSData data];
         [self save];
     }
+}
+
++(NSArray *)itemsWithSource:(YumSource *)source {
+    return [self itemsWithSource:source context:[PersistenceManager managedObjectContext]];
+}
+
++(NSArray *)itemsWithSource:(YumSource *)source context:(NSManagedObjectContext *)context {
+    NSManagedObjectID *sourceID = source.objectID;
+    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:NSStringFromClass(self.class)];
+        NSPredicate *pred = [NSPredicate predicateWithFormat:@"source == %@", source];
+    request.predicate = pred;
+    NSError *err = nil;
+    NSArray *filteredYums = [context executeFetchRequest:request error:&err];
+    if (err != nil) {
+        [NSException raise:YLCoreDataException format:@"Problem executing fetch request for %@. Error: %@", [self class], request];
+    }
+    return filteredYums;
 }
 
 @end
