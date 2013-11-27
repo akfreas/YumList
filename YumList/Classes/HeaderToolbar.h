e84dde8394807ad323517fa7c8e956f585cd5f10
Alex Freas <akfreas@gmail.com>
2013-11-26 23:29:17 -0500

    Removed headertoolbar.

--- YumList/Classes/HeaderToolbar.h
+++
@@ -1,5 +0,0 @@
-@interface HeaderToolbar : UIToolbar
-
-@property (copy, nonatomic) void(^leftNavigationButtonTappedAction)();
-@property (copy, nonatomic) void(^rightNavigationButtonTappedAction)();
-@end
