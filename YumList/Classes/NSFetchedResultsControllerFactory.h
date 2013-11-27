c38a2f2b0ba5b754d1e476c01b25fbab4bab9291
Alex Freas <akfreas@gmail.com>
2013-11-26 22:54:21 -0500

    [persistence helpers] Removed list items fetch controller factory methods. Sorting by completed on YumItems.

--- YumList/Classes/NSFetchedResultsControllerFactory.h
+++ YumList/Classes/NSFetchedResultsControllerFactory.h
@@ -1,8 +1,5 @@
 @interface NSFetchedResultsControllerFactory : NSObject
 
-
-+(NSFetchedResultsController *)fetchControllerForAllListItemsInContext:(NSManagedObjectContext *)context;
-+(NSFetchedResultsController *)fetchControllerForAllListItems;
 +(NSFetchedResultsController *)fetchControllerForAllYumItemsInContext:(NSManagedObjectContext *)context;
 +(NSFetchedResultsController *)fetchControllerForAllYumItems;
 
--- YumList/Classes/NSFetchedResultsControllerFactory.m
+++ YumList/Classes/NSFetchedResultsControllerFactory.m
@@ -1,35 +1,20 @@
 #import "NSFetchedResultsControllerFactory.h"
 #import "PersistenceManager.h"
-#import "ListItem.h"
 #import "YumItem.h"
 #import "YumSource.h"
 
 @implementation NSFetchedResultsControllerFactory
 
 
-+(NSFetchedResultsController *)fetchControllerForAllListItemsInContext:(NSManagedObjectContext *)context {
-    
-    NSSortDescriptor *listOrderSortDescriptor = [NSSortDescriptor sortDescriptorWithKey:@"listOrder" ascending:YES];
-    
-    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:NSStringFromClass([ListItem class])];
-    request.sortDescriptors = @[listOrderSortDescriptor];
-    NSFetchedResultsController *controller = [[NSFetchedResultsController alloc] initWithFetchRequest:request managedObjectContext:context sectionNameKeyPath:nil cacheName:nil];
-
-    return controller;
-}
-
-+(NSFetchedResultsController *)fetchControllerForAllListItems {
-    return [self fetchControllerForAllListItemsInContext:[PersistenceManager managedObjectContext]];
-}
-
 +(NSFetchedResultsController *)fetchControllerForYumItemsFromSource:(YumSource *)source {
     return [self fetchControllerForYumItemsFromSource:source inContext:[PersistenceManager managedObjectContext]];
 }
 +(NSFetchedResultsController *)fetchControllerForYumItemsFromSource:(YumSource *)source inContext:(NSManagedObjectContext *)context {
     NSSortDescriptor *sortDescriptor = [NSSortDescriptor sortDescriptorWithKey:@"title" ascending:YES];
+    NSSortDescriptor *completedSort = [NSSortDescriptor sortDescriptorWithKey:@"completed" ascending:NO];
     NSPredicate *predicate = [NSPredicate predicateWithFormat:@"source == %@", source];
     NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:NSStringFromClass([YumItem class])];
-    request.sortDescriptors = @[sortDescriptor];
+    request.sortDescriptors = @[sortDescriptor, completedSort];
     request.predicate = predicate;
     NSFetchedResultsController *controller = [[NSFetchedResultsController alloc] initWithFetchRequest:request managedObjectContext:context sectionNameKeyPath:nil cacheName:nil];
     return controller;
