02266340f8e8095d0e5c8e4046c5e92440f66b67
Alex Freas <akfreas@gmail.com>
2013-11-27 01:46:34 -0500

    [photo collection] Added photo collection view.

---
+++ YumList/Classes/YumPhotosCollectionView.h
@@ -0,0 +1,13 @@
+//
+//  YumPhotosCollectionView.h
+//  YumList
+//
+//  Created by Alexander Freas on 11/27/13.
+//  Copyright (c) 2013 Sashimiblade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YumPhotosCollectionView : UICollectionView
+
+@end
