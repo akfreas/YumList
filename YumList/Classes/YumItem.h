e20366bb30f54793dd218584561d2d19351a62a8
Alex Freas <akfreas@gmail.com>
2013-11-29 16:50:23 -0500

    [data model] Added rating, photos, externalsitetitle property to yumitem.

--- YumList/Classes/YumItem.h
+++ YumList/Classes/YumItem.h
@@ -2,27 +2,36 @@
 //  YumItem.h
 //  YumList
 //
-//  Created by Alexander Freas on 11/26/13.
+//  Created by Alexander Freas on 11/27/13.
 //  Copyright (c) 2013 Sashimiblade. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 #import <CoreData/CoreData.h>
 
-@class YumSource;
+@class YumPhoto, YumSource;
 
 @interface YumItem : NSManagedObject
 
 @property (nonatomic, retain) NSNumber * completed;
+@property (nonatomic, retain) NSString * externalSiteTitle;
 @property (nonatomic, retain) NSString * externalURL;
 @property (nonatomic, retain) NSString * externalYumID;
 @property (nonatomic, retain) NSData * image;
 @property (nonatomic, retain) NSString * imageURL;
 @property (nonatomic, retain) NSNumber * listOrder;
+@property (nonatomic, retain) NSNumber * rating;
 @property (nonatomic, retain) NSDate * syncDate;
 @property (nonatomic, retain) NSString * title;
-@property (nonatomic, retain) NSString * externalSiteTitle;
-@property (nonatomic, retain) NSNumber * rating;
 @property (nonatomic, retain) YumSource *source;
+@property (nonatomic, retain) NSSet *photos;
+@end
+
+@interface YumItem (CoreDataGeneratedAccessors)
+
+- (void)addPhotosObject:(YumPhoto *)value;
+- (void)removePhotosObject:(YumPhoto *)value;
+- (void)addPhotos:(NSSet *)values;
+- (void)removePhotos:(NSSet *)values;
 
 @end
--- YumList/Classes/YumItem.m
+++ YumList/Classes/YumItem.m
@@ -2,26 +2,28 @@
 //  YumItem.m
 //  YumList
 //
-//  Created by Alexander Freas on 11/26/13.
+//  Created by Alexander Freas on 11/27/13.
 //  Copyright (c) 2013 Sashimiblade. All rights reserved.
 //
 
 #import "YumItem.h"
+#import "YumPhoto.h"
 #import "YumSource.h"
 
 
 @implementation YumItem
 
 @dynamic completed;
+@dynamic externalSiteTitle;
 @dynamic externalURL;
 @dynamic externalYumID;
 @dynamic image;
 @dynamic imageURL;
 @dynamic listOrder;
+@dynamic rating;
 @dynamic syncDate;
 @dynamic title;
-@dynamic externalSiteTitle;
-@dynamic rating;
 @dynamic source;
+@dynamic photos;
 
 @end
