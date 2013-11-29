c5881d18c9186bab91464daa5e26caf9a0f63486
Alex Freas <akfreas@gmail.com>
2013-11-29 16:52:18 -0500

    [photo collection] added NSFetchedResultsController delegate methods to photo collection.

--- YumList/Classes/YumPhotosCollectionView.m
+++ YumList/Classes/YumPhotosCollectionView.m
@@ -6,18 +6,21 @@
 
 @implementation YumPhotosCollectionView {
     NSFetchedResultsController *fetchController;
+    NSMutableDictionary *sectionChanges;
+    NSMutableDictionary *rowChanges;
 }
 
 static NSString *CellIdentifier = @"PhotoCell";
 
 -(id)initWithYumItem:(YumItem *)item {
-    self = [super initWithFrame:CGRectZero];
+    UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc] init];
+    self = [super initWithFrame:CGRectZero collectionViewLayout:layout];
     if (self) {
         self.item = item;
+        [self registerClass:[YumPhotoCollectionCell class] forCellWithReuseIdentifier:CellIdentifier];
         self.delegate = self;
         self.dataSource = self;
         [self setupFetchController];
-        [self registerClass:[YumPhotoCollectionCell class] forCellWithReuseIdentifier:CellIdentifier];
     }
     return self;
 }
@@ -36,24 +39,45 @@ static NSString *CellIdentifier = @"PhotoCell";
     fetchController = [NSFetchedResultsControllerFactory fetchControllerForYumPhotosAttachedToItem:self.item];
     fetchController.delegate = self;
     [fetchController performFetch];
+    [self reloadData];
 }
 
 
 #pragma mark UICollectionView DataSource Methods
 
 -(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-    return [[[fetchController sections] objectAtIndex:section] count];
+    NSInteger numberOfRows = [[[fetchController sections] objectAtIndex:section] numberOfObjects];
+    return numberOfRows;
 }
 
 -(NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
-    return [[fetchController sections] count];
+    NSInteger numberOfSections = [[fetchController sections] count];
+    return numberOfSections;
 }
 
--(YumPhotoCollectionCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+-(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
     YumPhotoCollectionCell *cell = (YumPhotoCollectionCell *)[collectionView dequeueReusableCellWithReuseIdentifier:CellIdentifier forIndexPath:indexPath];
     YumPhoto *photo = [fetchController objectAtIndexPath:indexPath];
     cell.photo = photo;
     return cell;
+}
+
+-(CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
+    return CGSizeMake(130, 130);
+}
+
+#pragma mark NSFetchedResultsController Delegate Methods
+
+-(void)controllerDidChangeContent:(NSFetchedResultsController *)controller {
+    
+}
+
+-(void)controllerWillChangeContent:(NSFetchedResultsController *)controller {
+    
+}
+
+-(void)controller:(NSFetchedResultsController *)controller didChangeObject:(id)anObject atIndexPath:(NSIndexPath *)indexPath forChangeType:(NSFetchedResultsChangeType)type newIndexPath:(NSIndexPath *)newIndexPath {
+    
 }
 
 @end
